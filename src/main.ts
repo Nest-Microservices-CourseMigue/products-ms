594f5da918ecf424bb9707429d1e72bcf74d8d2a
MiguelGH6586 <miguel.agh97@gmail.com>
2024-11-11 18:49:28 -0500

    first crud of microservice products added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+import { lookup } from 'dns';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(envs.port ?? 3000);
+  logger.log(`App Running on port ${envs.port ?? 3000}`);
 }
 bootstrap();
